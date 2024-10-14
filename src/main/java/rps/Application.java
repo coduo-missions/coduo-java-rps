da9a9dc3df6f0818ee0602f1128215addc9ee898
GiSeok Kang <ime.rokz@gmail.com>
2024-10-14 22:18:07 +0900

    feat: main 코드 추가

--- src/main/java/rps/Application.java
+++ src/main/java/rps/Application.java
@@ -1,8 +1,15 @@
 package rps;
 
+import rps.util.controller.RspController;
+import rps.util.view.OutputView;
+
 public class Application {
+
+    private static RspController controller = new RspController();
 
     public static void main(String[] args) {
         // TODO : 기능 구현
+        OutputView.gameStart();
+        controller.game();
     }
 }
