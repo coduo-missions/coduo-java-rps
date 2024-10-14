afa0ed8e6a28c2fa0ca07b29416424641bac0ef2
GiSeok Kang <ime.rokz@gmail.com>
2024-10-15 01:18:11 +0900

    test: 승패 판정 및 유저 입력 테스트 추가

--- src/main/java/rps/Application.java
+++ src/main/java/rps/Application.java
@@ -1,15 +1,16 @@
 package rps;
 
-import rps.util.controller.RspController;
+import rps.util.controller.RpsController;
 import rps.util.view.OutputView;
 
 public class Application {
 
-    private static RspController controller = new RspController();
+    private static final RpsController controller = new RpsController();
 
     public static void main(String[] args) {
         // TODO : 기능 구현
         OutputView.gameStart();
         controller.game();
+        OutputView.gameEnd();
     }
 }
