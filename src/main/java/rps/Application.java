1915263a020a2789e4c1057f3338fd276d92637e
hwan2-99 <thh1233123@gmail.com>
2024-10-15 12:14:43 +0900

    feat: 승리조건 횟수 입력기능 구현

--- src/main/java/rps/Application.java
+++ src/main/java/rps/Application.java
@@ -1,8 +1,11 @@
 package rps;
 
+import rps.domain.WinningCount;
+import rps.view.InputView;
+
 public class Application {
 
     public static void main(String[] args) {
-        // TODO : 기능 구현
+        final WinningCount winningCount = new WinningCount(InputView.inputWinningCount());
     }
 }
---
+++ src/main/java/rps/view/InputView.java
@@ -0,0 +1,31 @@
+package rps.view;
+
+import rps.util.coduo.Console;
+
+public class InputView {
+    private static final String GAME_START_MESSAGE = "가위바위보 게임을 시작합니다.";
+    private static final String INPUT_WINNING_COUNT_MESSAGE = "승리를 위한 횟수를 입력하세요: ";
+    private static final String NUMBER_VALUE_ERROR_MESSAGE = "숫자값만 입력하세요.";
+
+    public InputView() {
+    }
+
+    public static int inputWinningCount() {
+        System.out.println(GAME_START_MESSAGE);
+        System.out.println();
+        System.out.print(INPUT_WINNING_COUNT_MESSAGE);
+        String inputValue = Console.input();
+        validateNumber(inputValue);
+        return Integer.parseInt(inputValue);
+    }
+
+    private static void validateNumber(final String inputValue) {
+        try {
+            Integer.parseInt(inputValue);
+        }catch (NumberFormatException e) {
+            throw new IllegalArgumentException(NUMBER_VALUE_ERROR_MESSAGE);
+        }
+    }
+
+
+}
